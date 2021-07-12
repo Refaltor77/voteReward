b9d08a0a76304595dd8ef008b579510af7f2e783
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-07-12 10:26:00 +0200

    Update voteAsync.php

--- src/refaltor/vote/async/voteAsync.php
+++ src/refaltor/vote/async/voteAsync.php
@@ -7,22 +7,23 @@ use pocketmine\plugin\Plugin;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 use pocketmine\utils\Internet;
+use refaltor\vote\Main;
 
 class voteAsync extends AsyncTask
 {
 
     private string $player;
-    private Plugin $plugin;
+    public string $key;
 
 
-    public function __construct(string $player, Plugin $plugin)
+    public function __construct(string $player, string $key)
     {
-        $this->plugin = $plugin;
         $this->player = $player;
+        $this->key = $key;
     }
 
     public function onRun(): void{
-        $key = $this->plugin->getConfig()->get('API_KEY');
+        $key = $this->key;
         $checking = Internet::getURL("https://minecraftpocket-servers.com/api/?object=votes&element=claim&key=$key&username=" . str_replace(" ", "+", $this->player));
         if($checking === "1") Internet::getURL("https://minecraftpocket-servers.com/api/?action=post&object=votes&element=claim&key=$key&username=" . str_replace(" ", "+", $this->player));
         $this->setResult($checking);
@@ -32,20 +33,21 @@ class voteAsync extends AsyncTask
 
         $checking = $this->getResult();
         $player = $server->getPlayer($this->player);
+        $plugin = Main::getInstance();
         if($player === null) return;
         switch($checking){
             case "0":
-                $message = $this->plugin->getConfig()->get("message_has_not_voted");
+                $message = $plugin->getConfig()->get("message_has_not_voted");
                 if ($message['enable']) $player->sendMessage($message['message']);
                 break;
             case "1":
-                $message = $this->plugin->getConfig()->get("message_player");
+                $message = $plugin->getConfig()->get("message_player");
                 if ($message['enable']) $player->sendMessage($message['message']);
 
-                $type = $this->plugin->getConfig()->get("reward_type");
+                $type = $plugin->getConfig()->get("reward_type");
                 switch (strtolower($type)){
                     case 'items':
-                        $array = $this->plugin->getConfig()->get("rewards_items");
+                        $array = $plugin->getConfig()->get("rewards_items");
                         foreach ($array as $string){
                             $item = explode("#", $string);
                             $item = Item::get($item[0], $item[1], $item[2])->setCustomName($item[3]);
@@ -55,7 +57,7 @@ class voteAsync extends AsyncTask
                         }
                         break;
                     case 'commands':
-                        $array = $this->plugin->getConfig()->get("rewards_commands");
+                        $array = $plugin->getConfig()->get("rewards_commands");
                         foreach ($array as $cmd){
                             $server->dispatchCommand($player, $cmd);
                         }
@@ -65,11 +67,11 @@ class voteAsync extends AsyncTask
                         break;
                 }
 
-                $message = $this->plugin->getConfig()->get("message_everyone");
+                $message = $plugin->getConfig()->get("message_everyone");
                 if ($message['enable']) Server::getInstance()->broadcastMessage(str_replace("{player}", $player->getName(), $message['message']));
                 break;
             default:
-                $message = $this->plugin->getConfig()->get("message_has_already_voted");
+                $message = $plugin->getConfig()->get("message_has_already_voted");
                 if ($message['enable']) $player->sendMessage($message['message']);
         }
     }
