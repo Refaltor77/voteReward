7db5c18df9e79e9dae2efc24d1ad68555ee459ee
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-07-12 10:25:28 +0200

    Update Main.php

--- src/refaltor/vote/Main.php
+++ src/refaltor/vote/Main.php
@@ -7,10 +7,17 @@ use refaltor\vote\command\vote;
 
 class Main extends PluginBase
 {
+    private static self $instance;
+
     public function onEnable()
     {
+        self::$instance = $this;
         $this->saveResource('config.yml');
         $array = $this->getConfig()->get('command');
         $this->getServer()->getCommandMap()->register($array['usage'], new vote($this));
+    }
+
+    public static function getInstance(): self{
+        return self::$instance;
     }
 }
